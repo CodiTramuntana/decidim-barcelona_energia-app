ed8fd06156e495e16cb4c43ad78ca78b014e05c3
Isaac Massot <isaac.mg@coditramuntana.com>
2018-12-17 13:10:37 +0100

    fix commented line

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   config.active_job.queue_adapter = :delayed_job
-  # config.active_job.queue_name_prefix = "decidim_salou_app_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "decidim_barcelona_energia_app_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
